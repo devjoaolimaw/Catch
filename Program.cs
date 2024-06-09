aff44cf91510b587e4deb1cda58c819cae6bb9b3
João Paulo <167019869+devjoaolimaw@users.noreply.github.com>
2024-06-09 19:04:04 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿try
+{ 
+
+    string[] linhas = File.ReadAllLines("Arquivos/arquivoleitura.txt");
+
+    foreach(string linha in linhas)
+    {
+        Console.WriteLine(linha);
+    }
+} 
+catch(FileNotFoundException ex)
+{
+    Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Arquivo não encontrado {ex.Message}");
+}
+catch(DirectoryNotFoundException ex)
+{
+    Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Caminha da pasta não encontrado. " + ex.Message);
+}
+catch(Exception ex)
+{ 
+   Console.WriteLine($"Ocorreu uma exceção generica. {ex.Message}");
+}
+finally
+{
+    Console.WriteLine("Chegou ate aqui");
+}
